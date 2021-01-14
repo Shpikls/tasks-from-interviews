dc77b4cbd705697afbbd90b2d328865a1c938a2a
Shpikl <shpikls@shpikls.ru>
2021-01-14 23:25:51 +0500

    fix solution & add readme.md

--- Task_03/index.js
+++ Task_03/index.js
@@ -1,11 +1,11 @@
 const solution = (number) => {
-	const arr = [];
+	let sum = 0;
 
 	for (let i = 1; i < number; i++) {
-		if (i % 5 === 0 || i % 3 === 0) arr.push(i)
+		if (i % 5 === 0 || i % 3 === 0) sum += i
 	}
 
-	return arr.reduce((accum, item) => accum + item, 0)
+	return sum
 }
 
 module.exports = solution
---
+++ Task_03/readme.md
@@ -0,0 +1,2 @@
+# CodeWars cata 6kyu
+[Link on kata](https://www.codewars.com/kata/514b92a657cdc65150000006)
